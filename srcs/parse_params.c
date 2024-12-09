38c9fd09aadc77426f1f6af9fe42c5450ace631f
Guillaume Ebersohl <guigui1991@live.fr>
2024-12-09 17:41:32 +0100

    Leak managed -> refacto parse_params for leak check and norm check

--- srcs/parse_params.c
+++ srcs/parse_params.c
@@ -6,29 +6,37 @@
 /*   By: gueberso <gueberso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 16:07:09 by gueberso          #+#    #+#             */
-/*   Updated: 2024/12/09 17:13:59 by gueberso         ###   ########.fr       */
+/*   Updated: 2024/12/09 17:40:51 by gueberso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	parse_params(int ac, char **av, int **stack_tmp)
+static void	parse_params_err_management(int i, char **av, int **stack_tmp)
 {
-	int		i;
-
-	*stack_tmp = malloc(sizeof(int) * (ac - 1));
-	if (!(*stack_tmp))
-		exit_with_error(ERR_MALLOC);
-	i = 1;
 	while (av[i])
 	{
 		if (!is_strdigit(av[i]))
+		{
+			free(*stack_tmp);
 			exit_with_error(ERR_NO_NUMERIC);
+		}
 		if (!check_out_of_range(av[i]))
+		{
+			free(*stack_tmp);
 			exit_with_error(ERR_OUT_OF_RANGE);
+		}
 		(*stack_tmp)[i - 1] = ft_atoi(av[i]);
 		i++;
 	}
+}
+
+int	parse_params(int ac, char **av, int **stack_tmp)
+{
+	*stack_tmp = malloc(sizeof(int) * (ac - 1));
+	if (!(*stack_tmp))
+		exit_with_error(ERR_MALLOC);
+	parse_params_err_management(1, av, stack_tmp);  
 	sort_tmp(*stack_tmp, ac - 1);
 	if (!check_duplicates(*stack_tmp, ac - 1))
 	{

36adb0ca020613c5f09b990727f5e9290f95aa77
Guillaume Ebersohl <guigui1991@live.fr>
2024-12-19 13:09:41 +0100

    parsing not leaking anymore

--- srcs/parse_string.c
+++ srcs/parse_string.c
@@ -6,7 +6,7 @@
 /*   By: gueberso <gueberso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 13:17:36 by gueberso          #+#    #+#             */
-/*   Updated: 2024/12/19 12:32:51 by gueberso         ###   ########.fr       */
+/*   Updated: 2024/12/19 13:09:07 by gueberso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,19 +33,16 @@ static void	parse_string_err_management(int size, char **spt, t_stacks *stacks)
 
 	if (size == 0)
 	{
+		free_stacks(stacks);
 		free(spt);
 		exit_with_error(ERR_EMPTY_STRING);
 	}
 	i = 0;
 	while (i < size)
 	{
-		if (!is_strdigit(spt[i]))
-		{
-			free_av1(spt, stacks->tmp.addr, size);
-			exit_with_error(ERR_NO_NUMERIC);
-		}
 		if (!check_out_of_range(spt[i]))
 		{
+			free_stacks(stacks);
 			free_av1(spt, stacks->tmp.addr, size);
 			exit_with_error(ERR_OUT_OF_RANGE);
 		}
@@ -70,6 +67,7 @@ int	parse_string(const char *str, t_stacks *stacks)
 	//sort_tmp(stacks);
 	if (!check_duplicates(stacks->tmp.addr, stacks->tmp.size))
 	{
+		free_av1(split, NULL, size);
 		free_stacks(stacks);
 		exit_with_error(ERR_DUPLICATE);
 	}
